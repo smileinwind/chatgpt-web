1d4b0805c3e37421e558ee161ae8294e2fbd9e80
91221377 <dengshupeng.yn@ccb.com>
2023-11-01 16:32:19 +0800

    启用vite代理

--- src/api/index.ts
+++ src/api/index.ts
@@ -45,7 +45,7 @@ export function fetchChatAPIProcess<T = any>(
   }
 
   return post<T>({
-    url: 'https://api-eta-silk.vercel.app/api/chat-process',
+    url: '/chat-process',
     data,
     signal: params.signal,
     onDownloadProgress: params.onDownloadProgress,
@@ -54,13 +54,13 @@ export function fetchChatAPIProcess<T = any>(
 
 export function fetchSession<T>() {
   return post<T>({
-    url: 'https://api-eta-silk.vercel.app/api/session',
+    url: '/session',
   })
 }
 
 export function fetchVerify<T>(token: string) {
   return post<T>({
-    url: 'https://api-eta-silk.vercel.app/api/verify',
+    url: '/verify',
     data: { token },
   })
 }
